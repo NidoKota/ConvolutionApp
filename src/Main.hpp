555aff6979b6fc97067a9c665689c461ddce1089
NidoKota <36328961+NidoKota@users.noreply.github.com>
2022-06-22 17:32:15 +0900

    update folder name

--- src/Main.hpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-
-#include "Signal/Include/Signal/Sample.hpp"
-#include "Signal/Include/Signal/SignalConsoleRenderer.hpp"
-
-//コマンドライン入力から信号を取得する
-void inputSignal(Signal& signalP);
